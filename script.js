9d3954ee9d97992328ad8857895f167240dcade4
Gopal <gopalkaran2021@gmail.com>
2021-08-31 08:48:20 +0530

    solved the color issue of footer text

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const footerItemList = document.querySelectorAll('[data-footer-item]');
 footerItemList.forEach(item => {
     item.addEventListener('click', ()=> {
-        console.log(item);
-        const [svg, info] = item.children;
-        console.log(svg)
-        console.log(info) 
+        // console.log(item);
+        // const [svg, info] = item.children;
+        // console.log(svg)
+        // console.log(info) 
         footerItemList.forEach(item => {
-            const [svg, info] = item.children;
-            svg.classList.remove('active-svg');
-            info.classList.remove('active-info');
-
+            // const [svg, info] = item.children;
+            // svg.classList.remove('active-svg');
+            // info.classList.remove('active-info');
+            // info.style.color= '#bbb';
+            item.classList.remove('active');
         })
-        svg.classList.add('active-svg');
-        info.classList.add('active-info');
+        // svg.classList.add('active-svg');
+        // info.classList.add('active-info');
+        // info.style.color = 'white';
+        item.classList.add('active');
     })
 })
