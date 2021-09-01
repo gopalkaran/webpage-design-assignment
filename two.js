8d49c81af5a3fac941492c9a699d521f5520c4f8
Gopal <gopalkaran2021@gmail.com>
2021-09-01 15:15:08 +0530

    added active user functionality on-click event

--- two.js
+++ two.js
@@ -1,20 +1,39 @@
 const footerItemList = document.querySelectorAll('[data-footer-item]');
 footerItemList.forEach(item => {
     item.addEventListener('click', ()=> {
-        // console.log(item);
-        // const [svg, info] = item.children;
-        // console.log(svg)
-        // console.log(info) 
         footerItemList.forEach(item => {
-            // const [svg, info] = item.children;
-            // svg.classList.remove('active-svg');
-            // info.classList.remove('active-info');
-            // info.style.color= '#bbb';
             item.classList.remove('active');
         })
-        // svg.classList.add('active-svg');
-        // info.classList.add('active-info');
-        // info.style.color = 'white';
         item.classList.add('active');
+    })
+})
+
+
+
+const userList = document.querySelectorAll('.user');
+// console.log(userList)
+userList.forEach(user => {
+    user.addEventListener('click', () => {
+        if(!user.classList.contains('active-user')){
+        // console.log(user);
+        const activeIcon = 
+        `<div class="active">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#FFFFFF"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+          </svg>
+        </div>`
+      var activeHolder = document.createElement("div");
+      activeHolder.innerHTML = activeIcon;
+      activeHolder.classList.add('active-holder');
+      user.appendChild(activeHolder);
+      user.classList.add('active-user');
+    }
     })
 })
